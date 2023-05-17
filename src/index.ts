8dab3017bbfad2544796a6c1bc11c2e161dc4c3c
Annylys Stiegelmeier Medeiros <annylys.medeiros@boxgroup.net.br>
2023-05-17 13:01:42 -0300

    Deploy no Heroku

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,45 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
-import * as express from "express";
+import express from "express";
 // import * as bodyParser from "body-parser";
-import * as cors from 'cors';
+import cors from 'cors';
 import routes from "./routes";
 const bodyParser = require('body-parser')
 
+require("dotenv").config();
+
+const config:any = {
+   "type": process.env.DB_POSTGRES_TYPE,
+   "host": process.env.DB_POSTGRES_HOST,
+   "port": process.env.DB_POSTGRES_PORT,
+   "username": process.env.DB_POSTGRES_USERNAME,
+   "password": process.env.DB_POSTGRES_PASSWORD,
+   "database": process.env.DB_POSTGRES_DATABASE,
+   "synchronize": true,
+   "logging": false,
+   "entities": [
+      "./src/entity/*"
+   ],
+   "migrations": [
+      "src/migration/**/*.ts"
+   ],
+   "subscribers": [
+      "src/subscriber/**/*.ts"
+   ],
+   "cli": {
+      "entitiesDir": "src/entity",
+      "migrationsDir": "src/migration",
+      "subscribersDir": "src/subscriber"
+   }
+}
+
 const app = express()
-createConnection()
+createConnection(config)
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use(routes)
  
-app.listen(3333)
+app.listen(3333, () => {
+  console.log('Servidor em Execução');
+});
